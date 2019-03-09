1741677dbbb68f1af01ca7c8b596636fb603d577
ali <ali@alineer.com>
2019-03-09 00:35:09 -0500

    set up for yapf and refactor to pythonic

--- scripts/metal-archives-scrape-fixer.py
+++ scripts/metal-archives-scrape-fixer.py
@@ -1,4 +1,33 @@
-from json_helper import load_list, dump_list
+import json
+
+def load_list(path):
+    with open(path, "r") as read_file:
+        band_list = json.load(read_file)
+    return band_list
+
+def dump_list(path, band_list):
+    with open(path, "w+") as write_file:
+        json.dump(band_list, write_file, indent=2) 
+
+def theme_fixer(band_list):
+    for i, band in enumerate(band_list):
+        lyrical_themes = band.get("lyrical_themes")
+        themes = lyrical_themes.split(", ")
+        
+        for theme in themes:
+            if " and " in theme or " / " in theme:
+                if " and " in theme:
+                    themes_split = theme.split(" and ")
+                else:
+                    themes_split = theme.split(" / ")
+                themes.remove(theme)
+                themes += themes_split # combine lists
+        
+        band['themes'] = themes # add themes to dict
+        band_list[i] = band # update in place
+
+        print("fixed themes for: " + band.get("name"))
+    return band_list
 
 def remove_early_later(styles): # takes a string
     style_list = styles.strip('(later)') # strip later, split earlier
@@ -19,7 +48,9 @@ def append_metal(style_list):
         s = ' Metal'
         if not ' metal' in style.lower():
             print('no metal')
-            if 'core' in style.lower() or 'punk' in style.lower() or 'rock' in style.lower() or 'djent' in style.lower() or 'hardcore' in style.lower() or 'crust' in style.lower():
+            if 'core' in style.lower() or 'punk' in style.lower() 
+                or 'rock' in style.lower() or 'djent' in style.lower() 
+                or 'hardcore' in style.lower() or 'crust' in style.lower():
                 print("in this stupid control " + style)
                 continue
             s1.append(style + s)
@@ -43,11 +74,12 @@ def genre_fixer(band_list):
         print('genre fixed for ' + band.get('name'))
     return band_list
 
+
 def main():
-    band_list = load_list('./json/items.json')
+    band_list = load_list('./json/test.json')
+    band_list = theme_fixer(band_list)
     band_list = genre_fixer(band_list)
     dump_list('./json/test-fix.json', band_list)
 
 if __name__ == '__main__':
     main()
-
